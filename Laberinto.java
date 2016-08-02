e12b41d4b11e806a27a0cd6e862cca913f489261
HiramRV <rub15044@uvg.edu.gt>
2016-08-01 22:29:34 -0600

    Clase Laberinto

--- Laberinto.java
+++ Laberinto.java
@@ -2,22 +2,11 @@ import java.lang.reflect.Array;
 
 public class Laberinto {
 
-	private Robot robotPrueba;
 	private Object[] T;
 	
+	
 	public Laberinto(int a, int b){
-		this.setRobotPrueba(new Robot(a,b));
-		this.setT(new Array[9][9]);
-	}
-
-	public Robot getRobotPrueba() {
-		return robotPrueba;
-	}
-
-
-
-	public void setRobotPrueba(Robot robotPrueba) {
-		this.robotPrueba = robotPrueba;
+		this.setT(new Array[a][b]);
 	}
 
 
@@ -28,16 +17,6 @@ public class Laberinto {
 	public void setT(Object[] t) {
 		T = t;
 	}	
-		
 	
-	/*Metodos */
-	public boolean VerificarDerecha(){
-		return false;
-	}
-	
-	public boolean BuscarPared(){
-		return false;
-	}
-	
-	
+
 }
