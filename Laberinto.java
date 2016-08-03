6613fa8c9210daffbca869169e6b603ae0fa6728
AMaybellP <pen15127@uvg.edu.gt>
2016-08-03 00:49:45 -0600

    Más laberintos

--- Laberinto.java
+++ Laberinto.java
@@ -50,6 +50,68 @@ public class Laberinto {
 
 		 * 
 		 */
+		 
+		 /* Object[8][6] inicio(0,3) fin(6,1)
+ 	T[0]= new Object[] {1,0,1,1,1,1,1,1};
+	T[1]= new Object[] {1,0,0,0,0,1,0,1};
+	T[2]= new Object[] {1,0,1,1,0,1,0,1};
+	T[3]= new Object[] {1,0,1,1,0,1,0,1};
+	T[4]= new Object[] {1,0,0,0,0,0,0,1};
+	T[5]= new Object[] {1,1,1,1,0,1,1,1};
+	*/
+	
+	/* Object[8][8] inicio(0,3) fin(5,0)
+	T[0]= new Object[] {1,1,1,1,1,1,1,1};
+	T[1]= new Object[] {1,0,1,0,0,0,0,1};
+	T[2]= new Object[] {0,0,0,0,1,1,0,1};
+	T[3]= new Object[] {1,1,0,1,1,1,0,1};
+	T[4]= new Object[] {1,0,0,0,0,0,1,1};
+	T[5]= new Object[] {1,1,1,1,1,0,1,1};
+	T[6]= new Object[] {1,0,0,0,0,0,0,1};
+	T[7]= new Object[] {1,1,1,0,1,1,1,1};
+	*/
+	
+	/* Object[8][14] inicio(2,7) fin(8,7)
+	T[0]= new Object[] {1,1,1,1,1,1,1,1};
+	T[1]= new Object[] {1,1,0,0,0,0,0,1};
+	T[2]= new Object[] {1,1,0,1,1,1,0,1};
+	T[3]= new Object[] {1,1,0,1,1,1,0,1};
+	T[4]= new Object[] {1,0,0,0,0,0,0,1};
+	T[5]= new Object[] {1,1,1,0,1,0,0,0};
+	T[6]= new Object[] {1,1,1,0,1,0,1,1};
+	T[7]= new Object[] {1,0,0,0,1,0,1,1};
+	T[8]= new Object[] {1,0,1,1,0,0,1,1};
+	T[9]= new Object[] {1,1,1,1,0,1,1,1};
+	T[10]= new Object[] {1,1,0,0,0,1,0,1};
+	T[11]= new Object[] {1,0,0,1,0,1,0,0};
+	T[12]= new Object[] {1,1,1,1,0,0,0,1};
+	T[13]= new Object[] {1,1,1,1,1,1,1,1};
+	*/
+	
+	/* Object[7][7] inicio(0,1) fin(3,0)
+	T[0]= new Object[] {1,1,1,1,1,1,1};
+	T[1]= new Object[] {1,0,0,1,0,0,1};
+	T[2]= new Object[] {1,0,1,1,0,0,1};
+	T[3]= new Object[] {0,0,0,0,0,0,1};
+	T[4]= new Object[] {1,1,1,1,0,1,1};
+	T[5]= new Object[] {1,0,0,0,0,0,1};
+	T[6]= new Object[] {1,0,1,1,1,1,1};
+	
+	*/
+	
+	/* Object[8][10] inicio(1,7) fin(8,0)
+	T[0]= new Object[] {1,1,1,1,1,1,1,1};
+	T[1]= new Object[] {0,0,0,0,1,1,1,1};
+	T[2]= new Object[] {1,1,1,0,0,0,0,1};
+	T[3]= new Object[] {1,0,0,1,0,1,1,1};
+	T[4]= new Object[] {1,1,0,0,0,0,0,1};
+	T[5]= new Object[] {1,1,1,1,1,1,0,1};
+	T[6]= new Object[] {1,1,0,0,0,0,0,1};
+	T[7]= new Object[] {1,0,0,1,0,1,1,1};
+	T[8]= new Object[] {1,1,1,1,0,0,0,0};
+	T[9]= new Object[] {1,1,1,1,1,1,1,1};
+	*/
+	
 		
 		
 	}
