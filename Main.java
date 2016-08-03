ca2c909868a9dd4b249fb886e11ed350a6e7ba42
hid15776 <hid15776@uvg.edu.gt>
2016-08-02 23:23:57 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,16 +11,6 @@ public class Main {
 		{
 			//System.out.println("Eje: "+bichito.getEje()+"   X: "+bichito.getCordenadaX()+"   Y: "+bichito.getCordenadaY());
 			bichito.avanzar();
-			//Impresion laberinto + posicion del bichito
-			for (int x=0; x < bichito.getLaberinto().getXf(); x++) {
-  				System.out.print("|");
-  				for (int y=0; y < bichito.getLaberinto().getYf(); y++) {
-   				 System.out.print (bichito.getLaberinto().getT());
-   				 System.out.print (bichito.getCordenadaX()+bichito.getCordenadaY());
-   					// if (y!=T[x].length-1) System.out.print("\t");
-  				}				
-  				System.out.println("|");
-			}
 			try
 			{
 				Thread.sleep(2000);
