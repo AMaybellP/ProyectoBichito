391ef8a43a3c89b3788db28097e9412071c23d7f
HiramRV <rub15044@uvg.edu.gt>
2016-09-01 16:47:57 -0600

    Update Prueba V2.c

--- Prueba V2.c
+++ Prueba V2.c
@@ -10,16 +10,21 @@ int main()
 	while(turn ==1){
 		//drive_rampStep(115,115);
 		if(ping_cm(9) > 11){ //no hay pared a la derecha
-            drive_goto(28,-27);//girar derecha
-            drive_goto(29,26); }                            
+            		drive_goto(28,-27);//girar derecha
+            		drive_goto(29,26); 
+			
+		}                            
      /*if(ping_cm(8) > 10){ //no hay pared enfrente
 			drive_goto(29,26); 
       }
       else{
       drive_goto(-28,27);} //girar izquierda
       }*/
-      
-		else{
-        drive_goto(-28,27);//girar izquierda
-  }   } }
+     		else{
+        		drive_goto(-28,27);//girar izquierda
+        		}  
+		
+	} 
+	
+}
 
