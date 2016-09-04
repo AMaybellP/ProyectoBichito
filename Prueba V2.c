4a8eb0b5c42b81d1f0669035e14f3dbadc87179c
HiramRV <rub15044@uvg.edu.gt>
2016-09-04 06:22:28 -0600

    Funcionamiento con dos sensores

--- Prueba V2.c
+++ Prueba V2.c
@@ -1,3 +1,6 @@
+/*
+  Blank Simple Project.c
+  http://learn.parallax.com/propeller-c-tutorials 
 */
 #include "abdrive.h"
 #include "ping.h"
@@ -12,19 +15,27 @@ int main()                                    // Main function
     freqout(1, 1, 38000);                       // Repeat for right detector
     irRight = input(2);
     if(irRight == 1){ // Si no hay pared a la derecha
+      drive_speed(32,32); //Avanza
+      pause(500);
       high(26);
       high(27);
-      drive_goto(25,-26); //Gira derecha
-      drive_speed(0,0);
-      drive_goto(29,26);  //Avanza
+      //drive_goto(25,-26); //Gira derecha
+      drive_speed(32,-32);
+      pause(700);
+      //drive_speed(0,0);
       low(26);
       low(27);
+      drive_speed(32,32); //Avanza
+      pause(1200);
+      //drive_speed(0,0);
     }      
-    if(irRight == 0 && ping_cm(8)<13){    //Si hay pared a la derecha y enfrente 
+    if(irRight == 0 && ping_cm(8)<12){    //Si hay pared a la derecha y enfrente 
       
       drive_speed(0,0);
-      drive_goto(-25,26); //Gira izquierda
-      pause(50); 
+     //Gira izquierda
+      drive_speed(-32,32);
+      pause(800);
+      drive_speed(0,0);
      }else{ //Si no hay pared enfrente
         drive_speed(32,32);
         pause(50);
